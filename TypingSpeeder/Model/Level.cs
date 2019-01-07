f4e8cb8f9a2724d781cc950b803591163ab3a2a8
Zygmunt Krawiec <zygmuntkrawiec@gmail.com>
2019-01-08 00:50:48 +0100

    Add Level enum

---
+++ TypingSpeeder/Model/Level.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypingSpeeder.Model
+{
+    public enum Level
+    {
+        Easy = 0,
+        Normal,
+        Hard
+    }
+}
