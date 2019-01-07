c55785125c5d2b51e9c766eead100e80d7a0f389
Zygmunt Krawiec <zygmuntkrawiec@gmail.com>
2019-01-08 00:59:29 +0100

    Add ScoreController class

---
+++ TypingSpeeder/Controllers/ScoreController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypingSpeeder.Controllers
+{
+    public static class ScoreCalculator
+    {
+        static int points = 0;
+        static int mishit = 0;
+
+        public static int Point()
+        {
+            return ++points;
+        }
+
+        public static int Missed()
+        {
+            return ++mishit;
+        }
+
+        public static void Reset()
+        {
+            points = 0;
+            mishit = 0;
+        }
+    }
+}
