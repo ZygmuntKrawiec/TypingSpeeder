c9e7a3d12d528f5e0d2eb60e337f7dfbe404c450
Zygmunt Krawiec <zygmuntkrawiec@gmail.com>
2019-01-08 00:52:02 +0100

    Add Mode class

---
+++ TypingSpeeder/Model/Mode.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypingSpeeder.Model
+{
+    public class Mode
+    {
+        public double Step { get; private set; }
+        public int Delay { get; private set; }
+        public int TimeDrop { get; private set; }
+        public int MinTime { get; private set; }
+        public Level Level { get; set; }
+        public TextType TextType { get; set; }
+        public int MaxNumberRange { get; set; }
+
+        public Mode(double step, int delay, int timeDrop, int minTime)
+        {
+            Step = step;
+            Delay = delay;
+            TimeDrop = timeDrop;
+            MinTime = minTime;
+        }
+    }
+}
