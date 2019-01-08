a53e97d21104e5e79a025ac25bfb5b2c412d8eee
Zygmunt Krawiec <zygmuntkrawiec@gmail.com>
2019-01-08 01:05:05 +0100

    Save changes into TypingSpeeder.csproj

--- TypingSpeeder/Providers/DataProvider.cs
+++ TypingSpeeder/Providers/DataProvider.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using TypingSpeeder.Model;
 
 namespace TypingSpeeder.Providers
 {
