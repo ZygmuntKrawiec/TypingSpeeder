c3d6eb26aac6e19ef428f8986a056cce6474f0b7
Zygmunt Krawiec <zygmuntkrawiec@gmail.com>
2019-01-19 23:23:03 +0100

    Add InvokeNextTextSample method in GameEngine class

--- TypingSpeeder/Engines/GameEngine.cs
+++ TypingSpeeder/Engines/GameEngine.cs
@@ -12,86 +12,97 @@ namespace TypingSpeeder.Engines
 {
     class GameEngine
     {
-     System.Timers.Timer timer;
-    int stepIndex;
-    public bool IsRunning { get; set; } = false;
+        System.Timers.Timer timer;
+        int stepIndex;
+        public bool IsRunning { get; set; } = false;
 
-    Mode gameMode = null;
-    public event Action<string> NextSampleAdded;
-    public event Action<double> NextStepMade;
+        Mode gameMode = null;
+        public event Action<string> NextSampleAdded;
+        public event Action<double> NextStepMade;
 
-    public GameEngine()
-    {
-        setTimer();
-    }
-
-    private void setTimer()
-    {
-        timer = new System.Timers.Timer();
-        timer.Interval = 1000;
-        timer.Elapsed += Timer_Elapsed;
-    }
-
-    private void Timer_Elapsed(object sender, ElapsedEventArgs e)
-    {
-        Task.Run(() => NextStepMade(gameMode.Step));
-        addNextSample();
-    }
-
-    private void addNextSample()
-    {
-        if (stepIndex == gameMode.Delay)
+        public GameEngine()
         {
-            var text = DataProvider.GenerateRandomValue(gameMode.Level, gameMode.TextType, gameMode.MaxNumberRange);
-            NextSampleAdded(text);
-            stepIndex = 0;
-            if (timer.Interval > gameMode.MinTime)
-                timer.Interval -= gameMode.TimeDrop;
-        }
-        else
-        {
-            stepIndex++;
-        }
-    }
-
-    public void SetGameMode(Level level)
-    {
-        var result = ModeController.GetModeValues(level);
-        gameMode = result;
-    }
-
-    public void SetGameTextType(TextType textType)
-    {
-        if (gameMode == null)
-        {
-            gameMode = ModeController.GetModeValues(Level.Normal);
-        }
-        gameMode.TextType = textType;
-    }
-
-    public void Start()
-    {
-        if (gameMode == null)
-        {
-            SetGameMode(Level.Normal);
-            SetGameTextType(TextType.Numbers);
+            setTimer();
         }
 
-        stepIndex = gameMode.Delay;
-        addNextSample();
-        timer.Start();
-        IsRunning = true;
-    }
+        private void setTimer()
+        {
+            timer = new System.Timers.Timer();
+            timer.Interval = 1000;
+            timer.Elapsed += Timer_Elapsed;
+        }
 
-    public void Stop()
-    {
-        timer.Stop();
-        IsRunning = false;
-    }
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Task.Run(() => NextStepMade(gameMode.Step));
+            addNextSample();
+        }
 
-    public bool ValidateValue(string answer, string value)
-    {
-        return value.Equals(answer);
+        private void addNextSample()
+        {
+            if (stepIndex == gameMode.Delay)
+            {
+                var text = DataProvider.GenerateRandomValue(gameMode.Level, gameMode.TextType, gameMode.MaxNumberRange);
+                NextSampleAdded(text);
+                stepIndex = 0;
+                if (timer.Interval > gameMode.MinTime)
+                    timer.Interval -= gameMode.TimeDrop;
+            }
+            else
+            {
+                stepIndex++;
+            }
+        }
+
+        public void SetGameMode(Level level)
+        {
+            var result = ModeController.GetModeValues(level);
+            gameMode = result;
+        }
+
+        public void SetGameTextType(TextType textType)
+        {
+            if (gameMode == null)
+            {
+                gameMode = ModeController.GetModeValues(Level.Normal);
+            }
+            gameMode.TextType = textType;
+        }
+
+        public void Start()
+        {
+            if (gameMode == null)
+            {
+                SetGameMode(Level.Normal);
+                SetGameTextType(TextType.Numbers);
+            }
+
+            stepIndex = gameMode.Delay;
+            addNextSample();
+            timer.Start();
+            IsRunning = true;
+        }
+
+        public void Stop()
+        {
+            timer.Stop();
+            IsRunning = false;
+        }
+
+        public bool ValidateValue(string answer, string value)
+        {
+            return value.Equals(answer);
+        }
+
+        /// <summary>
+        /// Forces to add next text sample.
+        /// </summary>
+        public void InvokeNextTextSample()
+        {
+            timer.Stop();
+            stepIndex = gameMode.Delay;
+            addNextSample();
+            timer.Start();
+        }
     }
-}
 }
